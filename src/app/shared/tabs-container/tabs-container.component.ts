0a88c6284c4c662e40410bdadb8fe24ad0dcfc30
Iqbal Farooqui <iqbal.farooqui@zillisor.com>
2022-08-14 17:40:25 -0400

    Add Register Form

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ContentChildren } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
@@ -6,13 +6,28 @@ import { TabComponent } from '../tab/tab.component';
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements AfterContentInit {
 
-  @ContentChildren(TabComponent) tabs = {}
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList()
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter(tab => tab.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false
+    })
+
+    tab.active = true;
+
+    return false;
   }
 
 }
